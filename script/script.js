4af80d42880c56dfeb1ef8baf2169fd312af60fe
juansesu3 <juanseu3@hotmail.com>
2021-09-15 16:45:00 -0500

    Iinicio con JavaScript

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let  cantidadDinero;
+let  monedaEntrada;
+let  monedaSalida;
+
+
+monedaEntrada = Number(prompt('Seleccione divisa'));
+cantidadDinero = Number(prompt('Ingrese cantidad de dinero'));
+monedaSalida = Number(prompt('Seleccione divisa'));
+
+
+
+
+let  monedaConvert = cantidadDinero*monedaSalida;
+console.log(monedaConvert);
+
+alert(`Total a Pagar es:${monedaConvert}`);
+
+
+
+
+
+
+
+
+
+
+
+
