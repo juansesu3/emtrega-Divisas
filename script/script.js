891de69b3b5509fc7c806cb8b6c95b6e00612a85
juansesu3 <juanseu3@hotmail.com>
2021-09-16 14:08:58 -0500

    avances divisas

--- script/script.js
+++ script/script.js
@@ -3,17 +3,18 @@ let  monedaEntrada;
 let  monedaSalida;
 
 
-monedaEntrada = Number(prompt('Seleccione divisa'));
-cantidadDinero = Number(prompt('Ingrese cantidad de dinero'));
-monedaSalida = Number(prompt('Seleccione divisa'));
 
+//monedaEntrada = Number(prompt('Seleccione divisa'));
+//cantidadDinero = Number(prompt('Ingrese cantidad de dinero'));
+//monedaSalida = Number(prompt('Seleccione divisa'));
 
-
+monedaEntrada =document.querySelector("#cantidad-dinero").value
+console.log(monedaEntrada)
 
 let  monedaConvert = cantidadDinero*monedaSalida;
 console.log(monedaConvert);
 
-alert(`Total a Pagar es:${monedaConvert}`);
+//alert(`Total a Pagar es:${monedaConvert}`);
 
 
 
